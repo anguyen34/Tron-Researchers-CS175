e0923c1eeedc4ef6a40dc7b427474c16aa4dc71a
anguyen34 <52220899+anguyen34@users.noreply.github.com>
2021-11-03 18:19:46 -0700

    Update agent_neural.py

--- agent_neural.py
+++ agent_neural.py
@@ -3,7 +3,17 @@
 # Brain Thai
 
 import numpy as np
-from colosseumrl.envs.blokus import BlokusEnvironment
+from colosseumrl.envs.blokus import BlokusEnvironment as be
+
+import gym
+from gym.spaces import Dict, Discrete, Box
+
+import ray
+from ray import tune
+from ray.rllib.agents.ppo import PPOTrainer, DEFAULT_CONFIG
+from ray.rllib.agents.dqn import DQNTrainer, DEFAULT_CONFIG
+
+
 
 class NeuralAgent:
   '''
