2016e24573be4002a028ae09fddc43f56dd9f7a5
anguyen34 <52220899+anguyen34@users.noreply.github.com>
2021-11-03 17:49:02 -0700

    Update agent_neural.py

--- agent_neural.py
+++ agent_neural.py
@@ -1,1 +1,13 @@
+# Trent Buckholz
+# Anthony Nguyen
+# Brain Thai
 
+import numpy as np
+from colosseumrl.envs.blokus import BlokusEnvironment
+
+class NeuralAgent:
+  '''
+  Agent that uses a neural network.
+  '''
+  def __init__(self):
+    pass
