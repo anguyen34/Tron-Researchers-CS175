1bef50b32a243bbb3f74754c8ccd65dc1cbca9ac
anguyen34 <52220899+anguyen34@users.noreply.github.com>
2021-11-03 17:48:29 -0700

    Update agent_mcsearch.py

--- agent_mcsearch.py
+++ agent_mcsearch.py
@@ -1,1 +1,13 @@
+# Trent Buckholz
+# Anthony Nguyen
+# Brain Thai
 
+import numpy as np
+from colosseumrl.envs.blokus import BlokusEnvironment
+
+class MCSearchAgent:
+  '''
+  Agent that uses Monte Carlo Search.
+  '''
+  def __init__(self):
+    pass
