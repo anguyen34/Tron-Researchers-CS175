47d25d5e31bb0346b873ff3fb250135335f03351
anguyen34 <52220899+anguyen34@users.noreply.github.com>
2021-11-22 16:08:03 -0800

    Update agent_forest.py

--- agent_forest.py
+++ agent_forest.py
@@ -70,11 +70,9 @@ class RandomForestAgent:
         b = self.state[0].flatten()
         board = np.pad(b, 2, 'constant', constant_values=-1)
         for r in range(len(old_players)):
-            board[board != old_players[r]] = -1
+            board[board != old_players[r] + 1 and board != 0] = -1
             act_to_str_np = np.array([act_to_str[actions[r]], self.state[1][old_players[r]], self.state[2][old_players[r]], self.state[3][old_players[r]]] + self.state[1] + self.state[2] + self.state[3])
             act_to_str_np = np.append(act_to_str_np, board)
-
-
             self.train_states[old_players[r]].append(act_to_str_np)
             self.train_rewards[old_players[r]].append(self.cumulative_rewards[old_players[r]])
 
@@ -139,7 +137,7 @@ class RandomForestAgent:
         #Flatten board
         b = self.state[0].flatten()
         board = np.pad(b, 2, 'constant', constant_values=-1)
-        board[board != pno] = -1
+        board[board != pno + 1 and board != 0] = -1
         for m in moves:
             # First 3 features to do are planned move, head, direction
             move_to_do = np.array([act_to_str[m], self.state[1][pno], self.state[2][pno], self.state[3][pno]] + self.state[1] + self.state[2] + self.state[3])
